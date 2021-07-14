477321de1039d5542a1bad41535362c9729a3cb6
Chanyoung Kim <44921488+kochanha@users.noreply.github.com>
2021-07-14 12:56:52 +0900

    updated automate pyntcloud

--- pyntcloud/pt.py
+++ pyntcloud/pt.py
@@ -1,31 +1,37 @@
 from pyntcloud import PyntCloud
 import copy
 import numpy as np
-import open3d as o3d
-
+import os
 from numpy import load
 import pandas as pd
 
-cloud = PyntCloud.from_file("bunny.obj")
+def meshtopc(name):
+    cloud = PyntCloud.from_file("obj/{}".format(name))
 
-#cloud.add_scalar_field("hsv")
+    voxelgrid_id = cloud.add_structure("voxelgrid", n_x=32, n_y=32, n_z=32)
+    new_cloud = cloud.get_sample("voxelgrid_nearest", voxelgrid_id=voxelgrid_id, as_PyntCloud=True)
+    new_cloud.to_file("npz/{}.npz".format(name))
+    data = load('npz/{}.npz'.format(name))
+    lst = data.files
+    arr = []
+    for item in lst:
+        arr = data[item]
+    arr = np.array(arr)
 
-voxelgrid_id = cloud.add_structure("voxelgrid", n_x=32, n_y=32, n_z=32)
-new_cloud = cloud.get_sample("voxelgrid_nearest", voxelgrid_id=voxelgrid_id, as_PyntCloud=True)
-new_cloud.to_file("bunny.npz")
-data = load('bunny.npz')
-lst = data.files
-arr = []
-for item in lst:
-    #print(item)
-    arr = data[item]
-arr = np.array(arr)
-# print(type(arr))
+    df = pd.DataFrame(arr)
+    df = df.iloc[:,:-1]
+    df = np.array(df)
 
-df = pd.DataFrame(arr)
-df = df.iloc[:,:-1]
-# print(df)
-df = np.array(df)
-#print(df)
-np.save("bunny.npy", df)
-print("Saved npy")
+    np.save("npy/{}.npy".format(name), df)
+
+def main():
+    path = "./obj"
+    file_list = os.listdir(path)
+
+    for i in range(len(file_list)):
+        meshtopc(file_list[i])
+        print(file_list[i])
+
+if __name__ == "__main__":
+    main()
+    print("Saved npy in npy folder")
