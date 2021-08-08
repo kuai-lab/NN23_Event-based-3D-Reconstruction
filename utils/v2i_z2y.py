8b64c1931b8a5f0f264e9e576b42677293920f62
dXDb <51734430+dXDb@users.noreply.github.com>
2021-08-08 18:09:29 +0900

    Update v2i_z2y.py

--- utils/v2i_z2y.py
+++ utils/v2i_z2y.py
@@ -2,18 +2,21 @@ import numpy as np
 import glob
 import cv2
 import os
+import natsort
 
-npz_flie = glob.glob("*.npz")
-video = cv2.VideoCapture("0000-1439.avi")
+folder_list = glob.glob("*_*")
 
-os.makedirs("./depth/data", exist_ok=True)
-os.makedirs("./depth/data", exist_ok=True)
+for folder in folder_list:
+    npz_flie = natsort.natsorted(glob.glob("./{}/*.npz".format(folder)))
 
+    os.makedirs("./{}_result/depth/data".format(folder), exist_ok=True)
+    os.makedirs("./{}_result/rgb/data".format(folder), exist_ok=True)
 
-for idx, npz in enumerate(npz_flie):
-    npz_file = np.load(npz)
-    np.save("depth/data/depth_{:010d}".format(idx), npz_file["depth_map"])
-    ret, frame = video.read()
-    cv2.imwrite("rgb/data/frame_{:010d}.png".format(idx), frame)
-
-video.release()
+    for idx, npz in enumerate(npz_flie):
+        npz_file = np.load(npz)
+        np.save("./{}_result/depth/data/depth_{:010d}".format(folder, idx), npz_file["depth_map"])
+        img = npz_file["normal_map"]
+        img = np.uint8((img - img.min()) / (img.max() - img.min()) * 255)
+        black_mask = np.where((img[:,:,0]==img[:,:,1])&(img[:,:,1]==img[:,:,2])&(img[:,:,0]==img[:,:,2]))
+        img[black_mask[0],black_mask[1],:]=0
+        cv2.imwrite("./{}_result/rgb/data/frame_{:010d}.png".format(folder, idx), img)
